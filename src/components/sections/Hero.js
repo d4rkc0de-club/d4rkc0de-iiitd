bb03f379a6fb05b6cfd66ba025bed8a9dc12def1
YashAgra <yash20551@iiitd.ac.in>
2021-12-03 02:06:11 +0530

    basic layout completed, font face remaining

--- src/components/sections/Hero.js
+++ src/components/sections/Hero.js
@@ -37,9 +37,10 @@ export default function Hero({
       >
         <Heading
           as="h1"
+          fontSize='60px'
           size="xl"
           fontWeight="bold"
-          color="primary.800"
+          color="primary.940"
           textAlign={["center", "center", "left", "left"]}
         >
           {title}
@@ -47,15 +48,17 @@ export default function Hero({
         <Heading
           as="h2"
           size="md"
-          color="primary.800"
+          fontSize='50px'
+          color="white"
           opacity="0.8"
-          fontWeight="normal"
+          fontWeight="600"
           lineHeight={1.5}
           textAlign={["center", "center", "left", "left"]}
+          fontFamily=""
         >
           {subtitle}
         </Heading>
-        <Link to={ctaLink}>
+        {/* <Link to={ctaLink}>
           <Button
             colorScheme="primary"
             borderRadius="8px"
@@ -75,9 +78,10 @@ export default function Hero({
           opacity="0.6"
         >
           Lorem Ipsum is simply dummy text of the printing and typesetting
-        </Text>
+        </Text> */}
       </Stack>
-      <Box w={{ base: "80%", sm: "60%", md: "50%" }} mb={{ base: 12, md: 0 }}>
+      {/* <Box w={{ base: "80%", sm: "60%", md: "50%" }} mb={{ base: 12, md: 0 }}> */}
+      <Box w="35%">
         <Image src={image} size="100%" rounded="1rem" shadow="2xl" />
       </Box>
     </Flex>
