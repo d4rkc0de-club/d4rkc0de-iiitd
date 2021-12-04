3cf41e763629bd3cdb8f268b6b2f2c9d4ed8cf67
Aditya Pratap Singh <adityapratapsingh51@gmail.com>
2021-12-04 10:31:55 +0530

    eslint

--- src/components/sections/Hero.js
+++ src/components/sections/Hero.js
@@ -1,14 +1,11 @@
 import React from "react";
-import { Link } from "react-router-dom";
 import PropTypes from "prop-types";
 import {
   Box,
-  Button,
   Flex,
   Image,
   Heading,
   Stack,
-  Text
 } from "@chakra-ui/react";
 
 export default function Hero({
