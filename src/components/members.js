26ffa0731639cdac15bd7ff72477603202140072
sc0rp10n-py <pragyan52yadav@gmail.com>
2021-12-07 04:33:22 +0530

    nada

--- src/components/members.js
+++ src/components/members.js
@@ -3,13 +3,21 @@ import { Flex, Box, Text, Image } from "@chakra-ui/react";
 
 export default function Members() {
     return (
-        <Flex direction="column" justify="center" align="center">
-            <Text>Members</Text>
-            <Box>
-                <Text>
-                    Members List
+        <>
+            <Flex direction="column" justify="center" align="center" mb="7rem">
+                <Text
+                    fontSize="5xl"
+                    color="primary.800"
+                    pb="35px"
+                    fontWeight={"bolder"}
+                >
+                    {">> "} Our Team {" <<"}
                 </Text>
-            </Box>
-        </Flex>
+
+                <Box>
+                    
+                </Box>
+            </Flex>
+        </>
     );
 }
--- src/pages/Landing.js
+++ src/pages/Landing.js
@@ -3,25 +3,26 @@ import React from "react";
 import Hero from "../components/sections/Hero";
 import LandingLayout from "../components/layouts/LandingLayout";
 import Events from "../components/events";
-
+import Members from "../components/members";
 import About from "../components/about";
 import Contact from "../components/contact";
 import Join from "../components/join";
 
 export default function Landing() {
-  return (
-    <>
-        <LandingLayout>
-            <Hero
-            title="root@d4rkc0de:~# \"
-            subtitle="Lorem Ipsum"
-            image="https://www.linkpicture.com/q/home_image.jpg"
-            />
-            <About />
-            <Events />
-            <Join />
-            <Contact/>
-        </LandingLayout>
-    </>
-  );
+    return (
+        <>
+            <LandingLayout>
+                <Hero
+                    title="root@d4rkc0de:~# \"
+                    subtitle="Lorem Ipsum"
+                    image="https://www.linkpicture.com/q/home_image.jpg"
+                />
+                <About />
+                <Events />
+                <Members />
+                <Join />
+                <Contact />
+            </LandingLayout>
+        </>
+    );
 }
